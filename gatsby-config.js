97a883ad62c8c91b2fecac974a0e6180c3a8ee1c
yann <yann.boisselier@gmail.com>
2020-01-30 20:22:23 +0100

    add purge css to reduce bulma css

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,6 +150,18 @@ config.plugins = [
       disable: !process.env.GATSBY_IFRAMELY_API_KEY, // When do you want to disable it ?
       src: `https://cdn.iframe.ly/embed.js?api_key=${process.env.GATSBY_IFRAMELY_API_KEY}`
     }
+  },
+  // keep this after css and scss plugins
+  {
+    resolve: `gatsby-plugin-purgecss`,
+    options: {
+      printRejected: true // Print removed selectors and processed file names
+      // develop: true, // Enable while using `gatsby develop`
+      // tailwind: true, // Enable tailwindcss support
+      // whitelist: ['whitelist'], // Don't remove this selector
+      // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+      // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    }
   }
 ];
 
