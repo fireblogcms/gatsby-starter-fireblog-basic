f2fce9dee441b501d4223178242d41ffe5a321ba
yann <yann.boisselier@gmail.com>
2020-01-30 19:20:37 +0100

    light gradient by default on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ function Header({
     );
   }
   return (
-    <section className={`hero is-primary ${classNames}`}>
+    <section className={`hero is-primary is-bold ${classNames}`}>
       <div className="hero-head">
         <Navbar />
       </div>
