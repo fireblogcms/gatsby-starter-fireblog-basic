c55b8ca3ec10ecba6315d4d2b4b01665b31288ee
yann <yann.boisselier@gmail.com>
2020-01-30 21:04:10 +0100

    use a gradient that reflect the name of fireblog

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ function Header({
     );
   }
   return (
-    <section className={`hero is-primary is-bold ${classNames}`}>
+    <section className={`hero ${classNames}`}>
       <div className="hero-head">
         <Navbar />
       </div>
